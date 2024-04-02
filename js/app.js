da22dd346c175180371a977c7e44e40a5d88e111
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-02 11:45:19 +0200

    Added tasks using JS

--- js/app.js
+++ js/app.js
@@ -4,7 +4,20 @@ const {createApp} = Vue;
 createApp({
     data() {
         return{
-            
+            tasks:[
+                {
+                    text: 'Fare gli esercizi',
+                    done: false
+                },
+                {
+                    text: 'Preparare il pranzo',
+                    done: false
+                },
+                {
+                    text: 'Lavare la macchina',
+                    done: false
+                }
+            ],
         }
     }
 
